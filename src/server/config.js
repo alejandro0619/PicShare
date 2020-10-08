2ff860a503c1a75bf616662b254652432a191bb7
SpahettiCodeDev <spaghetticodedev@gmail.com>
2020-10-08 05:10:05 -0430

    Router.get() requires a callback function but got a object string FIXED

--- src/server/config.js
+++ src/server/config.js
@@ -3,26 +3,36 @@ const path = require('path')
 const exphbs = require('express-handlebars')
 const morgan = require('morgan')
 const multer = require('multer')
+const express = require('express')
+const routes = require('../routes/index')
+
 //Server Config:
 module.exports = app => {
-    //Settings:
-  app.set('port', process.env.PORT || 3000)
-  app.set('views', path.join(__dirname, 'views'))
-  //handlebars basic config:
-  app.engine('hbs', exphbs({
-      defaultLayout : 'main',
-      partialsDir : path.join(app.get('views'), 'partials'),
-      layoutsDir: path.join(app.get('views', 'layouts')),
-      extname: '.hbs',
-      helpers: require('./helpers')
-  }))
-  app.set('view engine','.hbs')
+   app.set('port', process.env.PORT|| 3000)
+
+   app.set('views', path.join(__dirname, '../views'))
+
+    // handlebars basic config:
+   app.engine('.hbs', exphbs({
+      defaultLayout: 'main',
+      layoutsDir: path.join(app.get('views'), 'layouts'),
+      partialsDir: path.join(app.get('views'), 'partials'),
+      helpers: require('./helpers'),
+      extname: '.hbs'
+    })
+  )
+  //Setting view engine
+  app.set('view engine', '.hbs')
+
   //Middlewares:
   app.use(morgan('dev'))
   app.use(multer({
       dest: path.join(__dirname, '../public/upload/temp')
-  }).single())
-  //routes:
+  }).single('image'))
+  app.use(express.urlencoded({
+      extended: false
+  }))
+  app.use(express.json())
 
   //errorhandler:
     return app
