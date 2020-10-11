1e928f381d8f4a3c1b5f556bf30e1cfa75bf058e
SpahettiCodeDev <spaghetticodedev@gmail.com>
2020-10-11 12:36:34 -0430

    Upload images controller created

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,17 +1,31 @@
+// ! Importing modules:
 const path = require('path');
 const { randomNumber } = require('../helpers/libs');
-// ! Images [Upload, Delete, Render, Like, Comment] controllers
+const fs = require('fs-extra');
+
+// ! Images [Upload, Delete, Render, Like, Comment] controllers:
+
 const imgCtrl = {
+    
     index(req, res){
 
     },
-    create(req, res){
+    /* 
+    * Upload images (rename it to upload folder if it's an image)
+    *If it's not, send error message:
+    */
+     async create(req, res){
         const imageURL = randomNumber();
-        console.log(imageURL)
+        console.log(imageURL);
         const imageTempPath = req.file.path;
         const ext = path.extname(req.file.originalname).toLowerCase();
-        const targetpath = path.resolve(`src/public/upload/test${ext}`)
-        res.send('works');
+        const targetPath = path.resolve(`src/public/upload/${imageURL}${ext}`);
+        if(ext ==='.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
+           await fs.rename(imageTempPath, targetPath);
+           res.send('works');
+        }
+        res.send('didnt works');
+        
     },
     like(req, res){
 
